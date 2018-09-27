ff82069086e6c6480e9a01110afb7d74ac78178f
Askavolk <43269190+Askavolk@users.noreply.github.com>
2018-09-27 22:08:19 +0200

    Initial commit

---
+++ Assets/Scripts/RayCastHit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastHit : MonoBehaviour {
+
+    public GameObject pelota;
+    Rigidbody m_Rigidbody;
+    float m_Speed;
+    void Start()
+    {
+
+        //Fetch the Rigidbody component you attach from your GameObject
+        m_Rigidbody = GetComponent<Rigidbody>();
+        //Set the speed of the GameObject
+        m_Speed = 10.0f;
+    }
+
+    void update()
+    {
+
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.tag == "End")
+            {
+                pelota.transform.Translate(Vector3.forward * Time.deltaTime* m_Speed);
+                Debug.Log("Tocado");
+            }
+        }
+    }
+}
+
+ 
+ 
+    
